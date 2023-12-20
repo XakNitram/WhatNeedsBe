6906e6be42a8fe06ac7e385c5cc6374190fa8a4f
Tate Mioton <tatemioton@outlook.com>
2023-12-20 16:40:26 -0600

    Little HTMX Guide

--- app/blueprints/index.py
+++ app/blueprints/index.py
@@ -7,13 +7,25 @@ bp = Blueprint('index', __name__)
 
 @bp.route('/')
 def index():
+    # HTMX includes headers that can be used to identify its requests.
+    # If the request is coming from htmx, we serve only the content inside the <body>.
+    # Otherwise, the request is a normal browser request, say from a refresh.
     if is_htmx_request():
+        # HTMX takes the new <body> and replaces the current <body>, operating like an SPA.
         response = make_response(
             render_template("pages/index.html.j2")
         )
     else:
+        # If the request comes from a refresh or non-htmx request,
+        #  we inject the <body> into the includes.html.j2 template
+        #  that includes the <html> and <head> tags.
         response = make_response(
             render_page("pages/index.html.j2")
         )
+
+    # Unconfirmed, but I think this allows the browser to cache the
+    #  htmx SPA version the same as the non-htmx version of the page.
     response.headers.add("Vary", "HX-Request")
+
+    # See app/templates/pages/index.html.j2 for more info.
     return response
--- app/blueprints/task.py
+++ app/blueprints/task.py
@@ -11,9 +11,12 @@ bp = Blueprint('task', __name__, url_prefix="/task")
 @bp.route('/', methods=["GET"])
 def get():
     task_list = tuple(db.session.scalars(select(Task).order_by(Task.id.asc())))
-    # if not task_list:
-    #     return abort(500, "Not implemented.")
+
+    # HTMX wants endpoints to return html, so this is a html-api similar to xml apis.
+    # We can also use the htmx headers to decide to return data in the traditional json (or xml) format.
+    #  Certainly more work and not necessary.
     if is_htmx_request():
+        # This template only includes a <div> that can be slotted into a larger template with a htmx request.
         return render_template("components/task-list.html.j2", task_list=task_list)
     else:
         return make_response(jsonify({tid: task.id for tid, task in task_list}))
@@ -29,7 +32,15 @@ def create():
     db.session.commit()
 
     if is_htmx_request():
-        response = make_response(f"<p>{summary}</p>", 200)
+        # POST requests can also return html that htmx can slot into the page, but the hx-swap="none"
+        #  in the index template prevents that swapping from occuring, so this html is never seen.
+        #  I only include it now for adherence to POST standards that want a copy of the created resource returned,
+        #  but this can change in the future.
+        response = make_response(f"<p>{summary}</p>", 201)
+
+        # The HX-Trigger header is read by HTMX, which emits the event in the DOM.
+        # Not sure that I'm comfortable with this method, but it works, and it can be a documented feature of the API
+        #  used to trigger events client-side.
         response.headers['HX-Trigger'] = "TaskCreated"
         return response
     else:
